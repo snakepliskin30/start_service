46ca789794e541ca2180e17acb59966e2b3518f7
Rod <snakepliskin30@gmail.com>
2022-04-13 13:31:43 +0800

    fix usememo and usecallback

--- src/components/layout/Section.js
+++ src/components/layout/Section.js
@@ -3,28 +3,20 @@ import React, { useEffect, useState } from "react";
 import classes from "./Section.module.css";
 
 const Section = (props) => {
-  const [isSectionOpen, setSectionOpen] = useState(false);
-  console.log("isSectionOpen", isSectionOpen, props.title);
-
-  const clickSectionHeaderHandler = () => {
-    setSectionOpen((current) => !current);
-  };
-
-  useEffect(() => {
-    setSectionOpen(props.open);
-  }, [props.open]);
-
-  const iconPosition = isSectionOpen ? `${classes.header} ${classes.pointdown}` : `${classes.header}`;
+  console.log("section renders", props.title);
+  const iconPosition = props.children
+    ? `${classes.header} ${classes.pointdown}`
+    : `${classes.header}`;
 
   const noBtn = props.noBtn ? `${classes.noBtn}` : "";
   return (
     <div className={classes.section}>
-      <button className={`${iconPosition} ${noBtn}`} onClick={clickSectionHeaderHandler}>
+      <button className={`${iconPosition} ${noBtn}`} onClick={props.onClick}>
         {props.title}
       </button>
-      {isSectionOpen && <div className={classes.content}>{props.children}</div>}
+      <div className={classes.content}>{props.children}</div>
     </div>
   );
 };
 
-export default Section;
+export default React.memo(Section);
--- src/components/panel/PremiseInfo.js
+++ src/components/panel/PremiseInfo.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState, useCallback } from "react";
 import Accordion from "../layout/Accordion";
 import Field from "../ui/Field";
 import Section from "../layout/Section";
@@ -51,6 +51,7 @@ const premiseArray = [
 ];
 
 const PremiseInfo = () => {
+  console.log("premiseinfo renders");
   const [streetName, setStreetName] = useState("");
   const [city, setCity] = useState("");
   const [state, setState] = useState("GA");
@@ -61,12 +62,11 @@ const PremiseInfo = () => {
   const [searchResultShow, setSearchResultShow] = useState(false);
   const [searchInfoShow, setSearchInfoShow] = useState(false);
 
-  console.log("component ran");
   async function searchFormHandler(e) {
     e.preventDefault();
+    setApiCallFlag(true);
     setSearchResultShow(false);
     setSearchInfoShow(false);
-    setApiCallFlag(true);
 
     const Request = {};
     const Payload = {};
@@ -91,7 +91,11 @@ const PremiseInfo = () => {
     });
 
     const data = await response.json();
-    const premiseResults = Array.from(new Set(data.Payload.AddressInfo.map((s) => s.premiseNo))).map((premiseNum) => data.Payload.AddressInfo.find((d) => d.premiseNo === premiseNum));
+    const premiseResults = Array.from(
+      new Set(data.Payload.AddressInfo.map((s) => s.premiseNo))
+    ).map((premiseNum) =>
+      data.Payload.AddressInfo.find((d) => d.premiseNo === premiseNum)
+    );
     setPremiseResults(premiseResults);
     setSearchFormShow(false);
     setSearchResultShow(true);
@@ -121,107 +125,145 @@ const PremiseInfo = () => {
     Request.Payload = Payload;
 
     const requestString = JSON.stringify(Request);
-    console.log(requestString);
   }
 
   useEffect(() => {
     if (premiseResults.length > 0) {
+      setSearchFormShow(false);
       setSearchResultShow(true);
     }
   }, [premiseResults]);
+
+  const searchFormSectionClick = useCallback(() => {
+    setSearchFormShow((current) => !current);
+  }, []);
+  const searchResultSectionClick = useCallback(() => {
+    setSearchResultShow((current) => !current);
+  }, []);
+  const searchInfoSectionClick = useCallback(() => {
+    setSearchInfoShow((current) => !current);
+  }, []);
 
   return (
     <Fragment>
       {apiCallFlag && <Spinner />}
       <div className={classes.main}>
-        <Section title="Search" open={searchFormShow}>
-          <form className={classes.searchForm} onSubmit={searchFormHandler}>
-            <Input label="Street Name" id="streetName" value={streetName} onChange={setStreetName} />
-            <Input label="City" id="city" value={city} onChange={setCity} />
-            <Input label="State" id="state" value={state} onChange={setState} />
-            <InputNumber label="Zip" id="zip" options={{ blocks: [4] }} value={zip} onChange={setZip} />
-            <div className="btnGrp">
-              <ButtonSubmit>Submit</ButtonSubmit>
-              <ButtonCancel>Cancel</ButtonCancel>
-            </div>
-          </form>
-        </Section>
-
-        <Section title="Search Results" open={searchResultShow}>
-          <table id="searchResults" className="table table-striped">
-            <thead>
-              <tr>
-                <th>Street Name</th>
-                <th>Address Notes</th>
-                <th>City</th>
-                <th>State</th>
-                <th>Zip</th>
-              </tr>
-            </thead>
-            <tbody>
-              {premiseResults.map((premise, index) => (
-                <tr key={index} className={classes.resultsrow} onClick={getPremiseDetailsHandler.bind(null, premise)}>
-                  <td>{premise.addressLine1}</td>
-                  <td>{premise.AddressNotes}</td>
-                  <td>{premise.city}</td>
-                  <td>{premise.state}</td>
-                  <td>{premise.zipCode}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </Section>
-
-        <Section title="Premise Information" open={searchInfoShow}>
-          <div className={classes.premiseInfo}>
-            <Section title="Details" open={true} noBtn>
-              <div className={classes.premiseInfoDetails}>
-                <Field label="Meter Point Status" value="Active"></Field>
-                <Field label="Meter Status" value="On"></Field>
-                <Field label="RD/DC Meter" value="Yes"></Field>
-                <Field label="Revenue Class" value="Residential"></Field>
-                <Field label="Rate" value="Residential"></Field>
-                <Field label="PrePay Eligible" value="Yes"></Field>
+        <Section title="Search" onClick={searchFormSectionClick}>
+          {searchFormShow && (
+            <form className={classes.searchForm} onSubmit={searchFormHandler}>
+              <Input
+                label="Street Name"
+                id="streetName"
+                value={streetName}
+                onChange={setStreetName}
+              />
+              <Input label="City" id="city" value={city} onChange={setCity} />
+              <Input
+                label="State"
+                id="state"
+                value={state}
+                onChange={setState}
+              />
+              <InputNumber
+                label="Zip"
+                id="zip"
+                options={{ blocks: [4] }}
+                value={zip}
+                onChange={setZip}
+              />
+              <div className="btnGrp">
+                <ButtonSubmit>Submit</ButtonSubmit>
+                <ButtonCancel>Cancel</ButtonCancel>
               </div>
-            </Section>
-            <Section title="Pending Orders" open={true} noBtn>
-              <table id="searchResults" className="table table-striped">
-                <thead>
-                  <tr>
-                    <th>Date</th>
-                    <th>Type</th>
-                    <th>Bill Account</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>2022-04-29</td>
-                    <td>Connect</td>
-                    <td>78037-54259</td>
-                  </tr>
-                </tbody>
-              </table>
-            </Section>
-          </div>
+            </form>
+          )}
+        </Section>
 
-          <Section title="Pending Obligation" open={true} noBtn>
-            <table id="pendingObligation" className="table table-striped">
+        <Section title="Search Results" onClick={searchResultSectionClick}>
+          {searchResultShow && (
+            <table id="searchResults" className="table table-striped">
               <thead>
                 <tr>
-                  <th>Date</th>
-                  <th>Type</th>
-                  <th>Bill Account</th>
+                  <th>Street Name</th>
+                  <th>Address Notes</th>
+                  <th>City</th>
+                  <th>State</th>
+                  <th>Zip</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>2022-04-29</td>
-                  <td>Connect</td>
-                  <td>78037-54259</td>
-                </tr>
+                {premiseResults.map((premise, index) => (
+                  <tr
+                    key={index}
+                    className={classes.resultsrow}
+                    onClick={getPremiseDetailsHandler.bind(null, premise)}
+                  >
+                    <td>{premise.addressLine1}</td>
+                    <td>{premise.AddressNotes}</td>
+                    <td>{premise.city}</td>
+                    <td>{premise.state}</td>
+                    <td>{premise.zipCode}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
-          </Section>
+          )}
+        </Section>
+
+        <Section title="Premise Information" onClick={searchInfoSectionClick}>
+          {searchInfoShow && (
+            <div>
+              <div className={classes.premiseInfo}>
+                <Section title="Details" open={true} noBtn>
+                  <div className={classes.premiseInfoDetails}>
+                    <Field label="Meter Point Status" value="Active"></Field>
+                    <Field label="Meter Status" value="On"></Field>
+                    <Field label="RD/DC Meter" value="Yes"></Field>
+                    <Field label="Revenue Class" value="Residential"></Field>
+                    <Field label="Rate" value="Residential"></Field>
+                    <Field label="PrePay Eligible" value="Yes"></Field>
+                  </div>
+                </Section>
+                <Section title="Pending Orders" open={true} noBtn>
+                  <table id="searchResults" className="table table-striped">
+                    <thead>
+                      <tr>
+                        <th>Date</th>
+                        <th>Type</th>
+                        <th>Bill Account</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>2022-04-29</td>
+                        <td>Connect</td>
+                        <td>78037-54259</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </Section>
+              </div>
+
+              <Section title="Pending Obligation" open={true} noBtn>
+                <table id="pendingObligation" className="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>Date</th>
+                      <th>Type</th>
+                      <th>Bill Account</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>2022-04-29</td>
+                      <td>Connect</td>
+                      <td>78037-54259</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </Section>
+            </div>
+          )}
         </Section>
 
         <div className="btnGrp">
